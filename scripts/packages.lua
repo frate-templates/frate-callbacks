0e0d688a59aaee61578ba2c781974c06e2325272
lucas sprouole <lucas.sproule.42@gmail.com>
2023-12-14 19:41:46 -0500

    init dir

--- scripts/packages.lua
+++ scripts/packages.lua
@@ -1,9 +1,9 @@
 local cpm_str = ""
 for _, dep in pairs(project.dependencies) do
-  cpm_str = cpm_str .. "CPMAddPackage(\n" 
-  .. "NAME  "  .. dep.name .. "\n"
-  .. "GITHUB_REPOSITORY " .. dep.git_short .. "\n"
-  .. "GIT_TAG " .. dep.selected_version .. "\n"
+  cpm_str = cpm_str .. "CPMAddPackage(\n"
+  .. "\tNAME  "  .. dep.name .. "\n"
+  .. "\tGITHUB_REPOSITORY " .. dep.git .. "\n"
+  .. "\tGIT_TAG " .. dep.selected_version .. "\n"
   .. ")"
 end
 return cpm_str
