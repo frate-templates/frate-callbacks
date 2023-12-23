c0006318790397d04de3d28270ac1cea8c095f4a
lucas sprouole <lucas.sproule.42@gmail.com>
2023-12-22 23:49:41 -0500

    trying to have it somewhat cohesive

--- scripts/packages.lua
+++ scripts/packages.lua
@@ -2,7 +2,7 @@ local cpm_str = ""
 for _, dep in pairs(project.dependencies) do
   cpm_str = cpm_str .. "CPMAddPackage(\n"
   .. "\tNAME  "  .. dep.name .. "\n"
-  .. "\tGITHUB_REPOSITORY \"" .. dep.git .. "\"".. "\n"
+  .. "\tGIT_REPOSITORY \"" .. dep.git .. "\"".. "\n"
   .. "\tGIT_TAG " .. dep.selected_version .. "\n"
   .. ")"
 end
