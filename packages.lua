0066664a72c2757df9135805a936b76edb1ae0b9
lucas sprouole <lucas.sproule.42@gmail.com>
2023-12-31 23:57:41 -0500

    fixing watch command

--- packages.lua
+++ packages.lua
@@ -3,7 +3,7 @@ for _, dep in pairs(project.dependencies) do
   cpm_str = cpm_str .. "CPMAddPackage(\n"
   .. "\tNAME  "  .. dep.name .. "\n"
   .. "\tGIT_REPOSITORY \"" .. dep.git .. "\"".. "\n"
-  .. "\tGIT_TAG " .. dep.selected_version .. "\n"
+  .. "\tGIT_TAG " .. dep.version .. "\n"
   .. ")"
 end
 return cpm_str
